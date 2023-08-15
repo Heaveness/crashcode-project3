c32eb2a66bc837f92f9a94ef2529d030242d4456
kimmh891223 <kimmh891223@hotmail.com>
2023-08-14 20:49:07 -0400

    Jung In Kim: Setup queries for search bar(Not Implemented Yet on Nav.js) in resolvers and typeDefs and fixed small error with auth.js. Russell/Min Hwan (paired code): added delete functionality to the single post page.

--- client/src/components/pages/SinglePost.js
+++ client/src/components/pages/SinglePost.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_SINGLE_CODE } from '../../utils/queries';
+import { DELETE_CODE } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
 function SinglePost() {
     const { codeId } = useParams();
 
-    const { loading, data, error } = useQuery(QUERY_SINGLE_CODE, {
+    const { loading, data } = useQuery(QUERY_SINGLE_CODE, {
         variables: { codeId: codeId },
     });
+  
+    const [deleteCode] = useMutation(DELETE_CODE);
 
+
+    const handleDelete = async (event) => {
+        event.preventDefault();
+        try {
+          const { data } = await deleteCode({
+            variables: { codeId: codeId },
+          });
+          window.location.assign('/');
+        } catch (err) {
+          console.error(err);
+        }
+      }; 
+    
+    
     const singleCode = data?.singleCode || {};
 
     if (loading) {
@@ -26,6 +44,8 @@ function SinglePost() {
             <p className='card-header'>
                 Posted by {singleCode.username} on {singleCode.createdAt}
             </p>
+            {(singleCode.username === Auth.getUser().data.username) ? (<button className='delete-btn btn primary-btn bg-danger' onClick={handleDelete}>Delete Code?</button>) : (null)}
+            
         </div>
     )
 }
