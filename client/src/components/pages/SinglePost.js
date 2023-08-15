e282e255b4462f11739f245f48c5d35d3486804f
kimmh891223 <kimmh891223@hotmail.com>
2023-08-15 00:56:33 -0400

    Min Hwan: added front-end codes for update functionality in the SinglePost page which includes changing elements into inputs, and also canceling changes.

--- client/src/components/pages/SinglePost.js
+++ client/src/components/pages/SinglePost.js
@@ -27,18 +27,138 @@ function SinglePost() {
           console.error(err);
         }
       }; 
-
     const handleUpdate = async (event) => {
-      event.preventDefault();
-      try {
-        const { data } = await updateCode({
-          variables: { codeId: codeId, title: title, content: content},
-        });
-        window.location.assign('/');
-      } catch (err) {
-        console.error(err);
-      }
-    };   
+        event.preventDefault();
+        try {
+            // creating update submit button
+            const updateSubmitBtn = document.createElement('button');
+            const updateBtn = document.getElementById('updateBtn');
+            updateBtn.replaceWith(updateSubmitBtn);
+
+            // adding attributes to update submit button and adding event listener
+            updateSubmitBtn.setAttribute('class', 'btn btn-primary');
+            updateSubmitBtn.setAttribute('id', 'updateSubmitBtn');
+            updateSubmitBtn.innerHTML = 'Submit Changes';
+            updateSubmitBtn.addEventListener  ('click', async (event) => {
+                event.preventDefault();
+
+                const changedTitle = titleInput.value;
+                const changedContent = contentInput.value;
+                const changedProLang = proLangInput.value;
+
+                
+                try {
+                    const { data } = await updateCode({
+                    variables: { codeId: codeId, title: changedTitle, content: changedContent, programmingLanguage: changedProLang},
+                    });
+                    window.location.assign(`/codes/${codeId}`);
+                } catch (err) {
+                    console.error(err);
+                }
+
+            });
+
+            // creating cancel button
+            const updateCancelBtn = document.createElement('button');
+            const deleteBtn = document.getElementById('deleteBtn');
+            deleteBtn.replaceWith(updateCancelBtn);
+
+            // adding attributes to cancel button
+            updateCancelBtn.setAttribute('class', 'btn btn-danger');
+            updateCancelBtn.setAttribute('id', 'updateCancelBtn');
+            updateCancelBtn.innerHTML = 'Cancel Changes';
+
+            // adding event listener to cancel button
+            updateCancelBtn.addEventListener('click', () => {
+            updateSubmitBtn.replaceWith(updateBtn);
+            updateCancelBtn.replaceWith(deleteBtn);
+            titleCont.replaceChild(titleEl, titleInput);
+            contentCont.replaceChild(contentEl, contentInput);
+            proLangCont.replaceChild(progLangEl, proLangInput);
+            });
+
+            // creating variables for elements to be replaced
+            const titleEl = document.getElementById('titleEl');
+            const contentEl = document.getElementById('contentEl');
+            const progLangEl = document.getElementById('progLangEl');
+
+            const titleCont = document.getElementById('titleCont');
+            const contentCont = document.getElementById('contentCont'); 
+            const proLangCont = document.getElementById('proLangCont'); 
+
+            const titleValue = titleEl.textContent;
+            const contentValue = contentEl.textContent;
+
+            // replacing elements with inputs
+            const titleInput = document.createElement('textarea');
+            titleInput.setAttribute('style', 'font-size: 32px; font-weight: bold;');
+            titleInput.setAttribute('rows', '1');
+            titleInput.setAttribute('cols', '50');
+            titleInput.innerHTML = titleValue;
+            titleCont.replaceChild(titleInput, titleEl);
+
+          
+
+            const contentInput = document.createElement('textarea');
+            contentInput.setAttribute('style', 'font-size: 17.6px;');
+            contentInput.setAttribute('rows', '20');
+            contentInput.setAttribute('cols', '100');
+            contentInput.innerHTML = contentValue;
+            contentCont.replaceChild(contentInput, contentEl);
+
+            
+
+            // replacing element with dropdown menu
+            const proLangInput = document.createElement('select');
+            proLangInput.setAttribute('style', 'font-size: 24px; font-weight: bold; width: 200px; height: 60px;');
+            proLangInput.setAttribute('name', 'programmingLanguage');
+            proLangInput.setAttribute('id', 'pro-lang');
+
+            const option1 = document.createElement('option');
+            option1.setAttribute('value', 'Javascript');
+            option1.innerHTML = 'Javascript';
+            const option2 = document.createElement('option');
+            option2.setAttribute('value', 'Java');
+            option2.innerHTML = 'Java';
+            const option3 = document.createElement('option');
+            option3.setAttribute('value', 'Python');
+            option3.innerHTML = 'Python';
+            const option4 = document.createElement('option');
+            option4.setAttribute('value', 'C');
+            option4.innerHTML = 'C';
+            const option5 = document.createElement('option');
+            option5.setAttribute('value', 'C++');
+            option5.innerHTML = 'C++';
+            const option6 = document.createElement('option');
+            option6.setAttribute('value', 'C#');
+            option6.innerHTML = 'C#';
+            const option7 = document.createElement('option');
+            option7.setAttribute('value', 'PHP');
+            option7.innerHTML = 'PHP';
+            const option8 = document.createElement('option');
+            option8.setAttribute('value', 'Go');
+            option8.innerHTML = 'Go';
+            const option9 = document.createElement('option');
+            option9.setAttribute('value', 'Other');
+            option9.innerHTML = 'Other';
+
+            proLangInput.appendChild(option1);
+            proLangInput.appendChild(option2);
+            proLangInput.appendChild(option3);
+            proLangInput.appendChild(option4);
+            proLangInput.appendChild(option5);
+            proLangInput.appendChild(option6);
+            proLangInput.appendChild(option7);
+            proLangInput.appendChild(option8);
+            proLangInput.appendChild(option9);
+
+            proLangCont.replaceChild(proLangInput, progLangEl);
+
+            
+
+        } catch (err) {
+            console.error(err);
+        }};
     
     
     const singleCode = data?.singleCode || {};
@@ -49,16 +169,21 @@ function SinglePost() {
 
     return (
         <div>
-            <h2 className='card-header'>{singleCode.title}</h2>
-            <h4>Code Language: {singleCode.programmingLanguage}</h4>
-            <div className='card-body'>
-            <p>{singleCode.content}</p>
+            <div id ='titleCont'>
+                <h2 id ='titleEl' className='card-header'>{singleCode.title}</h2>
+            </div>
+            <div id ='proLangCont'>
+                <h4 id ='progLangEl'>Code Language: {singleCode.programmingLanguage}</h4>
+            </div>
+            <div id = 'contentCont' className='card-body'>
+                <p id ='contentEl'>{singleCode.content}</p>
             </div>
             <p className='card-header'>
                 Posted by {singleCode.username} on {singleCode.createdAt}
             </p>
-            {(singleCode.username === Auth.getUser().data.username) ? (<button className='delete-btn btn primary-btn bg-danger' onClick={handleDelete}>Delete Code?</button>) : (null)}
-            {(singleCode.username === Auth.getUser().data.username) ? (<button className='update-btn btn primary-btn' onClick={handleUpdate}>Update Code?</button>) : (null)}
+            {(singleCode.username === Auth.getUser().data.username) ? (<button id='updateBtn' className='update-btn btn btn-primary' onClick={handleUpdate}>Update Code</button>) : (null)}
+            {(singleCode.username === Auth.getUser().data.username) ? (<button id='deleteBtn' className='delete-btn btn btn-danger' onClick={handleDelete}>Delete Code</button>) : (null)}
+            
         </div>
     )
 }
